431c0662859f8627be10fc81a6fb1566a8dbf746
Hugo Visser <hugo@littlerobots.nl>
2025-06-16 18:10:42 +0200

    Add spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,26 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinx.serialization) apply false
     alias(libs.plugins.mavenPublish) apply false
     alias(libs.plugins.versionCatalogUpdate)
+    alias(libs.plugins.spotless)
+}
+
+configure<SpotlessExtension> {
+    kotlin {
+        target("**/*.kt")
+        ktfmt("0.54")
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktfmt("0.54")
+    }
+}
+
+tasks.register<Copy>("updateGitHooks") {
+    from("./scripts/pre-commit")
+    into("./.git/hooks")
 }
